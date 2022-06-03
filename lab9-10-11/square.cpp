2c6f782b6f9fd98eea609dff6dfd2579754969f1
daninil77 <danil_game1@mail.ru>
2022-06-03 23:02:28 +0300

    lab8

---
+++ lab9-10-11/square.cpp
@@ -0,0 +1,29 @@
+#include "square.h"
+#include <iostream>
+
+bool Square::isCursorOnFigure(QPoint pos)
+{
+    QRectF Square { upleftAngle.x(), upleftAngle.y(), side, side };
+    return Square.contains(pos);
+}
+
+void Square::draw(QPainter& painter, QPoint& pos)
+{
+    painter.setBrush(QColor(235, 0, 0, 255));
+    if (isCursorOnFigure(pos))
+        painter.setBrush(QColor(200, 0, 0, 255));
+    painter.drawRect(upleftAngle.x(), upleftAngle.y(), side, side);
+}
+
+void Square::info()
+{
+    std::cout << "\nFigure information: \n\tType: Square\n\tSide: " << side
+              << "\n\tUpper left angle point: (" << upleftAngle.x()
+              << "," << upleftAngle.y() << ")" << std::endl;
+}
+
+void Square::changeLocation(QPoint shift)
+{
+    upleftAngle += shift;
+}
+
